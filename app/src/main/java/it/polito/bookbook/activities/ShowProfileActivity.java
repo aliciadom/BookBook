6c94647ee90eb1d7d65c9d7c3f8ee82b6bdbbc0a
aliciadom <valerio.domanico@studenti.polito.it>
2019-02-01 16:08:12 +0100

    initial commit

---
+++ app/src/main/java/it/polito/bookbook/activities/ShowProfileActivity.java
@@ -0,0 +1,28 @@
+package it.polito.bookbook.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import it.polito.bookbook.R;
+
+public class ShowProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("DEBUG_TEST","ON_SAVE_INSTANCE");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("DEBUG_TEST","ON_RESTORE_INSTANCE");
+    }
+}
