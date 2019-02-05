4a23acdd5a71c1dc232f90da5cf796efb4e0467e
aliciadom <valerio.domanico@studenti.polito.it>
2019-02-05 15:52:48 +0100

    support from taking a picture profile or picking from gallery

--- app/src/main/java/it/polito/bookbook/activities/EditProfileActivity.java
+++ app/src/main/java/it/polito/bookbook/activities/EditProfileActivity.java
@@ -3,30 +3,47 @@ package it.polito.bookbook.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import it.polito.bookbook.R;
 
 public class EditProfileActivity extends AppCompatActivity{
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_GALLERY = 2;
     private static final String LOG_TAG = EditProfileActivity.class.getSimpleName();
     private static final String sharedPrefFile = "it.polito.bookbook";
+    private Uri profilePictureUri = null;
     private SharedPreferences mPreferences;
     private EditText
             editText_name,
             editText_email,
             editText_bio;
+    private ImageView imageView_profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        String name, email, bio;
+        String name, email, bio, picUriString;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
@@ -34,14 +51,34 @@ public class EditProfileActivity extends AppCompatActivity{
         editText_name = findViewById(R.id.editText_name);
         editText_email = findViewById(R.id.editText_email);
         editText_bio = findViewById(R.id.editText_bio);
+        imageView_profilePicture = findViewById(R.id.imageView_profilePicture);
 
         name = mPreferences.getString("NAME",getResources().getString(R.string.name));
         email = mPreferences.getString("EMAIL",getResources().getString(R.string.email));
         bio = mPreferences.getString("BIO",getResources().getString(R.string.bio));
+        picUriString = mPreferences.getString("PICTURE_PROFILE",null);
 
         editText_name.setText(name);
         editText_email.setText(email);
         editText_bio.setText(bio);
+
+        if(picUriString!=null){
+            String[] projection = {MediaStore.MediaColumns.DATA};
+            profilePictureUri = Uri.parse(picUriString);
+            Cursor cursor = getContentResolver().query(profilePictureUri, projection, null, null, null);
+            if(cursor!=null){
+                imageView_profilePicture.setImageURI(profilePictureUri);
+                cursor.close();
+            }
+            else{
+                /*Here we handle the error, clear the PICTURE_PROFILE preference*/
+                Log.e(LOG_TAG,picUriString + " invalid Uri");
+                SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+                preferencesEditor.putString("PICTURE_PROFILE",null);
+                preferencesEditor.apply();
+            }
+
+        }
     }
 
     @Override
@@ -58,12 +95,24 @@ public class EditProfileActivity extends AppCompatActivity{
         return true;
     }
 
-    private void savePreferences(String name, String email, String bio){
+    private void savePreferences(String name, String email, String bio, Uri uri){
         SharedPreferences.Editor preferencesEditor = mPreferences.edit();
         preferencesEditor.putString("NAME",name);
         preferencesEditor.putString("EMAIL",email);
         preferencesEditor.putString("BIO",bio);
+        if(uri!=null)
+            preferencesEditor.putString("PICTURE_PROFILE",uri.toString());
         preferencesEditor.apply();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            imageView_profilePicture.setImageURI(profilePictureUri);
+        }else if(requestCode == REQUEST_IMAGE_GALLERY && resultCode == RESULT_OK && data != null){
+            profilePictureUri = data.getData();
+            imageView_profilePicture.setImageURI(profilePictureUri);
+        }
     }
 
     private void returnShowProfileActivity() {
@@ -71,11 +120,52 @@ public class EditProfileActivity extends AppCompatActivity{
         savePreferences(
         editText_name.getText().toString(),
         editText_email.getText().toString(),
-        editText_bio.getText().toString());
+        editText_bio.getText().toString(),
+                profilePictureUri);
 
         Intent returnIntent = new Intent();
         setResult(Activity.RESULT_OK,returnIntent);
         finish();
     }
+
+    public void takePictureFromCamera(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                String error_message = getResources().getString(R.string.camera_error_message);
+                Toast.makeText(getApplicationContext(), error_message, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,"it.polito.bookbook.android.fileprovider",photoFile);
+                profilePictureUri = photoURI;
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        }
+    }
+
+    public void selectPictureFromGallery(View view){
+        Intent selectPictureIntent = new Intent(Intent.ACTION_PICK);
+        selectPictureIntent.setType("image/*");
+        startActivityForResult(selectPictureIntent, REQUEST_IMAGE_GALLERY);
+    }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",java.util.Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
+
 
 }
--- app/src/main/java/it/polito/bookbook/activities/ShowProfileActivity.java
+++ app/src/main/java/it/polito/bookbook/activities/ShowProfileActivity.java
@@ -2,12 +2,16 @@ package it.polito.bookbook.activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import it.polito.bookbook.R;
@@ -16,11 +20,13 @@ public class ShowProfileActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = ShowProfileActivity.class.getSimpleName();
     private static final String sharedPrefFile = "it.polito.bookbook";
+    static final int REQUEST_EDITPROFILE = 0;
     private SharedPreferences mPreferences;
     private TextView
             textView_name,
             textView_email,
             textView_bio;
+    private ImageView imageView_profilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +38,7 @@ public class ShowProfileActivity extends AppCompatActivity {
         textView_name = findViewById(R.id.textView_name);
         textView_email = findViewById(R.id.textView_email);
         textView_bio =  findViewById(R.id.textView_bio);
-
+        imageView_profilePicture = findViewById(R.id.imageView_profilePicture);
     }
 
     @Override
@@ -57,18 +63,39 @@ public class ShowProfileActivity extends AppCompatActivity {
 
     private void launchEditProfileActivity() {
         Intent intent = new Intent(this, EditProfileActivity.class);
-        startActivityForResult(intent,0);
+        startActivityForResult(intent,REQUEST_EDITPROFILE);
     }
 
     private void loadPreferences(){
 
-        String name, email, bio;
+        String name, email, bio, picUriString;
         name = mPreferences.getString("NAME",getResources().getString(R.string.name));
         email = mPreferences.getString("EMAIL",getResources().getString(R.string.email));
         bio = mPreferences.getString("BIO",getResources().getString(R.string.bio));
+        picUriString = mPreferences.getString("PICTURE_PROFILE",null);
 
         textView_name.setText(name);
         textView_email.setText(email);
         textView_bio.setText(bio);
+
+        if(picUriString != null){
+
+            String[] projection = {MediaStore.MediaColumns.DATA};
+            Uri profilePictureUri = Uri.parse(picUriString);
+
+            Cursor cursor = getContentResolver().query(profilePictureUri, projection, null, null, null);
+            if(cursor!=null){
+                imageView_profilePicture.setImageURI(profilePictureUri);
+                cursor.close();
+            }
+            else{
+                /*Here we handle the error, clear the PICTURE_PROFILE preference*/
+                Log.e(LOG_TAG,picUriString + " invalid Uri");
+                SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+                preferencesEditor.putString("PICTURE_PROFILE",null);
+                preferencesEditor.apply();
+            }
+
+        }
     }
 }
